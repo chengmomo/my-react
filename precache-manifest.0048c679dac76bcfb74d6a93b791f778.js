63f5c0bfda318f7dd0e50a24ef754392f29292a2
18252030586 <z923891099@icloud.com>
2019-08-29 11:27:53 +0800

    Updates

---
+++ precache-manifest.0048c679dac76bcfb74d6a93b791f778.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7321d1b56a902b6dfdc83b9b37d6bd",
+    "url": "/my-react/index.html"
+  },
+  {
+    "revision": "f60e40b58a77fff89b2b",
+    "url": "/my-react/static/css/0.9b269089.chunk.css"
+  },
+  {
+    "revision": "7a9ca18b74f3b946a0e4",
+    "url": "/my-react/static/css/3.ef014991.chunk.css"
+  },
+  {
+    "revision": "ef9a2e0507383539fe39",
+    "url": "/my-react/static/css/4.69665863.chunk.css"
+  },
+  {
+    "revision": "b86948035b302cc9e094",
+    "url": "/my-react/static/css/5.200d5a53.chunk.css"
+  },
+  {
+    "revision": "6e5a204a81d623335256",
+    "url": "/my-react/static/css/6.c1ebbab7.chunk.css"
+  },
+  {
+    "revision": "065580418df5074b60d7",
+    "url": "/my-react/static/css/7.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "165b1d5ab9465d9cb51e",
+    "url": "/my-react/static/css/8.024a120a.chunk.css"
+  },
+  {
+    "revision": "521fe84beb9a8f915f05",
+    "url": "/my-react/static/css/main.8f4bd211.chunk.css"
+  },
+  {
+    "revision": "f60e40b58a77fff89b2b",
+    "url": "/my-react/static/js/0.e6aa5c5a.chunk.js"
+  },
+  {
+    "revision": "7a9ca18b74f3b946a0e4",
+    "url": "/my-react/static/js/3.5908804b.chunk.js"
+  },
+  {
+    "revision": "ef9a2e0507383539fe39",
+    "url": "/my-react/static/js/4.cba986bc.chunk.js"
+  },
+  {
+    "revision": "b86948035b302cc9e094",
+    "url": "/my-react/static/js/5.d4ba91e6.chunk.js"
+  },
+  {
+    "revision": "6e5a204a81d623335256",
+    "url": "/my-react/static/js/6.35054922.chunk.js"
+  },
+  {
+    "revision": "065580418df5074b60d7",
+    "url": "/my-react/static/js/7.5eba8591.chunk.js"
+  },
+  {
+    "revision": "165b1d5ab9465d9cb51e",
+    "url": "/my-react/static/js/8.e9a69f9c.chunk.js"
+  },
+  {
+    "revision": "521fe84beb9a8f915f05",
+    "url": "/my-react/static/js/main.6694caa3.chunk.js"
+  },
+  {
+    "revision": "2f387390432ea597ae4a",
+    "url": "/my-react/static/js/runtime~main.337caebd.js"
+  }
+]);
